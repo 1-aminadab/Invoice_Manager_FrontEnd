443abfcb548a57d3c357c97f99cfc65bfd591aff
1-aminadab <4ethiochild@gmail.com>
2024-05-23 09:37:13 +0300

    modifie invoice sheet

--- app/components/templates/InvoiceSheet.tsx
+++ app/components/templates/InvoiceSheet.tsx
@@ -121,55 +121,55 @@ function InvoiceSheet() {
     </CardHeader>
     <CardContent  ref={componentRef} className="p-6 text-sm">
       <div className="grid gap-3"  id="content-to-download">
-        <div className="font-semibold">Order Details</div>
+        <div className="font-semibold">Invoice Details</div>
         <ul className="grid gap-3">
           <li className="flex items-center justify-between">
             <span className="text-muted-foreground">
-              Glimmer Lamps x <span>2</span>
+              Game 1 x <span>2</span>
             </span>
-            <span>$250.00</span>
+            <span>250.00 birr</span>
           </li>
           <li className="flex items-center justify-between">
             <span className="text-muted-foreground">
-              Aqua Filters x <span>1</span>
+              Game 2 x <span>1</span>
             </span>
-            <span>$49.00</span>
+            <span>490000.00 birr</span>
           </li>
         </ul>
         <Separator className="my-2" />
         <ul className="grid gap-3">
           <li className="flex items-center justify-between">
             <span className="text-muted-foreground">Subtotal</span>
-            <span>$299.00</span>
+            <span>299.00 birr</span>
           </li>
           <li className="flex items-center justify-between">
-            <span className="text-muted-foreground">Shipping</span>
-            <span>$5.00</span>
+            <span className="text-muted-foreground">Delivery</span>
+            <span>5.00 birr</span>
           </li>
           <li className="flex items-center justify-between">
             <span className="text-muted-foreground">Tax</span>
-            <span>$25.00</span>
+            <span>25.00 birr</span>
           </li>
           <li className="flex items-center justify-between font-semibold">
             <span className="text-muted-foreground">Total</span>
-            <span>$329.00</span>
+            <span>329.00 birr</span>
           </li>
         </ul>
       </div>
       <Separator className="my-4" />
       <div className="grid grid-cols-2 gap-4">
         <div className="grid gap-3">
-          <div className="font-semibold">Shipping Information</div>
+          <div className="font-semibold">Develiver Information</div>
           <address className="grid gap-0.5 not-italic text-muted-foreground">
-            <span>Liam Johnson</span>
-            <span>1234 Main St.</span>
-            <span>Anytown, CA 12345</span>
+            <span>Wesen Michael</span>
+            <span>1234 </span>
+            <span>sole, CA 12345</span>
           </address>
         </div>
         <div className="grid auto-rows-max gap-3">
           <div className="font-semibold">Billing Information</div>
           <div className="text-muted-foreground">
-            Same as shipping address
+            Same as delivery address
           </div>
         </div>
       </div>
@@ -190,7 +190,7 @@ function InvoiceSheet() {
           <div className="flex items-center justify-between">
             <dt className="text-muted-foreground">Phone</dt>
             <dd>
-              <a href="tel:">+1 234 567 890</a>
+              <a href="tel:">+251 234 567 890</a>
             </dd>
           </div>
         </dl>
@@ -202,7 +202,7 @@ function InvoiceSheet() {
           <div className="flex items-center justify-between">
             <dt className="flex items-center gap-1 text-muted-foreground">
               <CreditCard className="h-4 w-4" />
-              Visa
+             Cbe
             </dt>
             <dd>**** **** **** 4532</dd>
           </div>
